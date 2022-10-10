a81fbb80305584a83e1c463f75e64a2ebe24aaec
RiivoI <113367497+RiivoI@users.noreply.github.com>
2022-10-10 18:21:51 +0300

    kodutöö08

---
+++ Kodutöö08/script.js
@@ -0,0 +1,17 @@
+let nuppElement = document.getElementById('nupp');
+let nimiElement = document.getElementById('nimi');
+let hinnangElement = document.getElementById('hinnang');
+
+nuppElement.addEventListener('click', hindaNime);
+
+function hindaNime() {
+    let nimePikkus = nimiElement.value.length;
+    if (nimePikkus == 0) {
+        hinnangElement.innerHTML = ' ';
+    }
+    if (nimePikkus < 6) {
+        hinnangElement.innerHTML = 'Küll sul on ilus lühike nimi';
+    } else {
+        hinnangElement.innerHTML = 'Küll sul on suurepärane pikk nimi';
+    }
+}
